e0d11e5d21933e0d6764a3a6ce517fa4e7005dd4
Shimona Prabhu <0718shimona@gmail.com>
2022-10-17 16:56:19 -0700

    tooltip changes and country aggregation of pie

--- Homework2/smprabhu/app.js
+++ Homework2/smprabhu/app.js
@@ -31,10 +31,8 @@ d3.queue()
     
 
     // createMap(width, width * 4 / 5);
-    createPie(width, height);
     createBar(width, height);
     // drawMap(geoData, data, currentYear, currentDataType);
-    drawPie(data, currentYear);
     drawBar(data, currentDataType, "");
 
     var countryMap = {};
@@ -81,16 +79,7 @@ d3.queue()
       //     drawBar(data, currentDataType, country);
   })
 
-    d3.select("#year")
-        .property("min", currentYear)
-        .property("max", extremeYears[1])
-        .property("value", currentYear)
-        .on("input", () => {
-          currentYear = +d3.event.target.value;
-          // drawMap(geoData, data, currentYear, currentDataType);
-          drawPie(data, currentYear);
-          highlightBars(currentYear);
-        });
+
 
     d3.selectAll('input[name="data-type"]')
         .on("change", () => {
@@ -134,6 +123,92 @@ d3.queue()
         tooltip 
             .html(`
               <p>Country: ${data.country}</p>
+              <p>${formatDataType(dataType)}: ${dataValue}</p>
+              <p>Year: ${data.year || d3.select("#year").property("value")}</p>
+              ${percentage}
+            `)
+      }
+    }
+  });
+
+  d3.queue()
+  .defer(d3.json, "//unpkg.com/world-atlas@1.1.4/world/50m.json")
+  .defer(d3.csv, "./data/output-2.csv", function(row) {
+    return {
+      emissionsPerCapita: +row["Emissions Per Capita"],
+      region: row.Region,
+      year: +row.Year
+    }
+  })
+  .await(function(error, mapData, data) {
+    if (error) throw error;
+
+    var extremeYears = d3.extent(data, d => d.year);
+    
+    var currentYear = extremeYears[0];
+    
+    var currentDataType = d3.select('input[name="data-type"]:checked')
+                            .attr("value");
+    // var geoData = topojson.feature(mapData, mapData.objects.countries).features;
+
+    var width = +d3.select(".chart-container")
+                   .node().offsetWidth;
+    var height = 300;
+
+    
+
+    // createMap(width, width * 4 / 5);
+    createPie(width, height);
+    // drawMap(geoData, data, currentYear, currentDataType);
+    drawPie(data, currentYear);
+
+    
+    
+
+
+    d3.select("#year")
+        .property("min", currentYear)
+        .property("max", extremeYears[1])
+        .property("value", currentYear)
+        .on("input", () => {
+          currentYear = +d3.event.target.value;
+          // drawMap(geoData, data, currentYear, currentDataType);
+          drawPie(data, currentYear);
+          highlightBars(currentYear);
+        });
+
+ 
+    d3.selectAll("svg")
+        .on("mousemove touchmove", updateTooltip);
+
+    function updateTooltip() {
+      var tooltip = d3.select(".tooltip");
+      var tgt = d3.select(d3.event.target);
+      var isCountry = tgt.classed("country");
+      var isBar = tgt.classed("bar");
+      var isArc = tgt.classed("arc");
+      var dataType = d3.select("input:checked")
+                       .property("value");
+      var units = dataType === "emissions" ? "thousand metric tons" : "metric tons per capita";
+      var data;
+      var percentage = "";
+      if (isCountry) data = tgt.data()[0].properties;
+      if (isArc) {
+        data = tgt.data()[0].data;
+        percentage = `<p>Percentage of total: ${getPercentage(tgt.data()[0])}</p>`;
+      }
+      if (isBar) data = tgt.data()[0];
+      tooltip
+          .style("opacity", +(isCountry || isArc || isBar))
+          .style("left", (d3.event.pageX - tooltip.node().offsetWidth / 2) + "px")
+          .style("top", (d3.event.pageY - tooltip.node().offsetHeight - 10) + "px");
+      if (data) {
+        var dataValue = data[dataType] ?
+          data[dataType].toLocaleString() + " " + units :
+          "Data Not Available";
+        tooltip 
+            .html(`
+              <p>Region: ${data.region}</p>
               <p>${formatDataType(dataType)}: ${dataValue}</p>
               <p>Year: ${data.year || d3.select("#year").property("value")}</p>
               ${percentage}
