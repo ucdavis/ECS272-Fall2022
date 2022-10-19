31990fe3ab0e57a7afda9d53ba7638abb5205a58
yuya737 <ykawakam@uoregon.edu>
2022-10-18 23:34:21 -0700

    adding HW2

---
+++ Homework2/yuya/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <a-layout class="main-page">
+        <Navigation />
+        <a-layout>
+            <a-layout style="padding: 0 24px 24px">
+                <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+                    <router-view></router-view>
+                </a-layout-content>
+            </a-layout>
+        </a-layout>
+    </a-layout>
+</template>
+
+<script setup lang="ts">
+import Navigation from "./views/components/navbar.vue"
+// import SideBar from "./views/components/sidebar.vue"
+
+</script>
+
+<style scoped lang="css">
+.main-page{
+    height:100%;
+}
+</style>
