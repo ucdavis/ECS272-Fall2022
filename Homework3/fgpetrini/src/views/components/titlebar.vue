c8a00e22449cc91373f0d2bf495671a6aa5157fd
Francesco Petrini <francescopetrini@Francescos-MacBook-Air.local>
2022-10-28 15:43:01 -0700

    Homework3 start

---
+++ Homework3/fgpetrini/src/views/components/titlebar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header">
+        <h1>Airline Satisfaction Visualization - By Francesco Petrini</h1>
+    </div>
+</template>
+
+<style>
+
+.header h1{
+    background-color: black;
+    color: white;
+    padding: 15px;
+    padding-left: 10px;
+}
+
+.header h3{
+    padding-left: 10px;
+}
+
+</style>
