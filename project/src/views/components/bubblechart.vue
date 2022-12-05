80534507308bfaf23b4d8f7ebd552249d41c5828
zenoWZH <wangzhouhao94@gmail.com>
2022-12-05 14:43:56 -0800

    add page 3

--- project/src/views/components/bubblechart.vue
+++ project/src/views/components/bubblechart.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="bubbleChartContainer" :id="myChartID">
+        <svg></svg>
     </div>
 </template>
 
@@ -13,13 +14,13 @@ export default {
     data() {
         return {
             bubbleData: [
-                {name: 1910, count: 14},
-                {name: 1920, count: 20},
-                {name: 1930, count: 42},
-                {name: 1940, count: 94},
-                {name: 1950, count: 344},
-                {name: 1960, count: 54},    
-                {name: 1970, count: 124}
+                {key: 1910, value: 14},
+                {key: 1920, value: 20},
+                {key: 1930, value: 42},
+                {key: 1940, value: 94},
+                {key: 1950, value: 344},
+                {key: 1960, value: 54},    
+                {key: 1970, value: 124}
             ],
             arc: null,
             arcs: [],
@@ -28,6 +29,7 @@ export default {
             arcLabel: null,
             width: 300,
             height: 300,
+            diameter: 300
         }
     },
     props: {
@@ -42,75 +44,90 @@ export default {
     mounted() {
         /* We have access to our HTML defined in Template*/
         // D3 CODE CALLED HERE\
-        this.init(this.pieData);
-        this.drawPie(this.pieData);
+        this.init(this.bubbleData);
+        this.drawBubble(this.bubbleData);
     },
     methods: {
+        classes(root) {
+					var classes = [];                                                                                        //存储结果的数组
+					/*自定义递归函数
+					 *
+					 * 第二个参数指传入的json对象
+					 * */
+					function recurse(name, node) {
+					    if (node.children)                                                                                   //如果有孩子结点 （这里的children不是自带的，是json里面有的）
+					    {
+					    	node.children.forEach(function(child) {                                                          //将孩子结点中的每条数据
+					    		recurse(node.name, child); })
+					    }
+					    else {classes.push({ className: node.name, value: node.size})};                                     //如果自身是孩子结点的，将内容压入数组
+					}
+					recurse(null, root);
+					return {children: classes};                                                                             //返回所有的子节点  （包含在children中）                                                                          
+				},
         init(data) {
             let id = '#'+this.myChartID;
-            this.arc =  d3.arc()
-                        .innerRadius(20)
-                        .outerRadius(Math.min(this.width, this.height) / 2 - 1)
-
+            this.bubble = d3.pack()                                  //初始化包图
+                            //.radius(this.diameter)                       
+                            .size([this.diameter, this.diameter])	                    //设置范围
+                            .padding(1.5);    
+                                                                            //设置间距
             this.color = d3.scaleOrdinal()
-                        .domain(data.map(d => d.date))
+                        .domain(data.map(d => d.key))
                         .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse())
-           
-            this.pie =  d3.pie()
-                    .sort(null)
-                    .value(d => d.count)
+
+            let svg = d3.select(id).select("svg").attr("viewBox", [0, 0, this.width, this.height]);
             
-            const radius = Math.min(this.width, this.height) / 2 * 0.8;
-            this.arcLabel = d3.arc().innerRadius(radius).outerRadius(radius)
-
-            let svg = d3.select(id).append("svg").attr("viewBox", [0, 0, this.width, this.height]);
-            svg.append("g").attr("id", this.myChartID+"pieGroup").attr("transform", `translate(${this.width / 2},${this.height / 2})`)
-            svg.append("g").attr("id", this.myChartID+"pieLabelGroup").attr("transform", `translate(${this.width / 2},${this.height / 2})`)
         },
-        drawPie(data) {
+        drawBubble(data) {
             let id = '#'+this.myChartID;
-            let arcs = this.pie(data);
-            let svg = d3.select(id+"pieGroup");
-
-            let vueThis = this;
-            svg.attr("stroke", "white")
-                .selectAll("path")
-                .data(arcs)
-                .join("path")
-                .attr("fill", d => this.color(d.data.date))
-                .attr("d", this.arc);
-                //.on("click",function(e,d,i){
-                //    console.log("CLICKED THIS", e,d,i)
-                //    vueThis.$emit("TellParentSTuff", d)
-                //});
+            let svg = d3.select(id).select("svg")
+            var result = data;
+			/*以下是字符串拼接*/
+			var startString = "{\"name\": \"flare\",\"children\": [";                      //开头字符串
+			result.forEach(function(dude){                                                 //遍历result并且拼接
+				startString+="{\"name\":\""+dude.key+"\",\"size\":"+dude.value+"},";
+			})
+			/*去除最后一个末尾的逗号，这个逗号会影响后面JSON.parse的使用*/
+			startString = startString.substring(0,startString.length-1);
+			/*拼接尾部字符串*/
+			startString+="]}";
+			/*将拼接好的字符串转换成json对象*/
+			var json2 = JSON.parse(startString);
+			/*绘图部分*/
+			console.log(this.classes(json2));
+			var node = svg.selectAll(".node")
+	                      .data(this.bubble(this.classes(json2))                                                                //绑定数据（配置结点）
+	                      .filter(function(d) { return !d.children; }))                                                      //数据过滤，满足条件返回自身（没孩子返回自身，有孩子不返回，这里目的是去除父节点）
+	 	                  .enter().append("g")
+	                      .attr("class", "node")
+	                      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });                  //设定g移动
+				
+				node.append("title")
+				    .text(function(d) { return d.className + ": " + (d.value); });             //设置移入时候显示数据   数据名和值
+				
+				node.append("circle")
+				    .attr("r", function(d) { return d.r;})                                     //设置圆的半径
+				    .style("fill", function(d) { return this.color(d.value); });              //为圆形涂色
+				
+				node.append("text")
+				    .attr("dy", ".3em")
+				    .style("text-anchor", "middle")                                            //设置文本对齐
+				    .text(function(d) { return d.className.substring(0, d.r / 3); })          //根据半径的大小来截取对应长度字符串（很重要）
+                    .on("mouseover", function(d) {
+                        d3.select(this)
+                        .transition()
+                        .duration(100)
+                        .attr("font-size", "18px")
+                    })
+                    .on("mouseout", function(d) {
+                        d3.select(this)
+                        .transition()
+                        .duration(100)
+                        .attr("font-size", "12px")
+                    });
                 
-
-            d3.select(id+"pieLabelGroup")
-                .attr("font-family", "sans-serif")
-                .attr("font-size", "12px")
-                .attr("text-anchor", "middle")
-                .selectAll("text")
-                .data(arcs)
-                .join("text")
-                .attr("transform", d => `translate(${this.arcLabel.centroid(d)})`)
-                //.selectAll("tspan")
-                //.join("tspan")
-                //.attr("x", 0)
-                //.attr("y", (_, i) => `${i * 1.1}em`)
-                //.attr("font-weight", (_, i) => i ? null : "bold")
-                .text(function (d) { return d.data.date })
-                .on("mouseover", function(d) {
-                    d3.select(this)
-                    .transition()
-                    .duration(100)
-                    .attr("font-size", "18px")
-                })
-                .on("mouseout", function(d) {
-                    d3.select(this)
-                    .transition()
-                    .duration(100)
-                    .attr("font-size", "12px")
-                });
+            let vueThis = this;
         }
     }
 }
--- project/src/views/pages/third.vue
+++ project/src/views/pages/third.vue
@@ -1,5 +1,103 @@
 <template>
     <div>
-        <h1>This is the 3rd view</h1>
+        <h1>Summary of Companies' involved OSS projects</h1>
+    </div>
+    <div class="column middle">
+        <BubbleChart v-if="dataExists" myChartID="middlepie"></BubbleChart>
+    </div>
+    <div class="bottom bar">
+        <BarChart v-if="dataExists" @selectedyear="updateYear" myChartID="bottombar" :myBarchartData=myBarData></BarChart>
     </div>
 </template>
+
+<script setup>
+    import BubbleChart from '../components/bubblechart.vue';
+    import BarChart from '../components/barchart.vue';
+    import testData from "../../assets/data/test.json";
+</script>
+<script>
+import * as d3 from "d3";
+export default {
+    data(){
+        return {
+            dataExists: false,
+            myBarData: Array,
+            //data_person = d3.csvParse(FileAttachment().text(), d3.autoType)
+            //data_title = d3.csvParse(FileAttachment().text(), d3.autoType)
+            //actorGroups : Array,
+            dateselected : {},
+            titleGroups : {},
+            titleGroups_selected : {},
+            //fdata_person : Array,
+            fdata_title : Array,
+            title_nameindex : Object,
+            title_idindex : Object,
+            title_radar : String,
+        }
+    },
+    components: {
+        BarChart,
+        //PieChart,
+        BubbleChart
+        //Sunburst,
+        //NodeTree,
+        //RadarChart
+    },
+    created(){
+        this.myBarData = testData.data;
+        //console.log("Test Bardata", this.myBarData);
+        this.dataExists = true;
+    },
+    mounted(){
+        
+    },
+    methods:{
+        updateYear(data){
+            console.log("Year changed!", data)
+            this.dateselected[0] = data[0];
+            this.dateselected[1] = data[1];
+            console.log("Year updated", this.dateselected)
+        },
+    }
+}
+</script>
+
+<style scoped>
+    body {
+    margin: 0;
+    }
+    .column {
+    float: left;
+    padding: 10px;
+    }
+
+    .column.side {
+    width: 50%;
+    height: 50%;
+    }
+
+    .column.middle {
+    width: 50%;
+    height: 50%;
+    }
+    .bottombar{
+        width: 100%;
+        height: 30%;
+    }
+    .card {
+    background-color: white;
+    padding: 5px;
+    margin-top: 5px;
+    }
+
+    .row:after {
+    content: "";
+    display: table;
+    clear: both;
+    }
+@media screen and (max-width: 600px) {
+  .column.side, .column.middle {
+    width: 100%;
+  }
+}
+</style>
