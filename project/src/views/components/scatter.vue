076f259ea74db0ccbf3439d38e2be20b33615a63
ZhennanWu <znwu@ucdavis.edu>
2022-12-06 10:47:22 -0800

    trend first prototype

--- project/src/views/components/scatter.vue
+++ project/src/views/components/scatter.vue
@@ -6,75 +6,83 @@
 <script>
 import * as d3 from "d3";
 import testData from "../../assets/data/test.json";
-export default{
-    name : "ScatterPlot",
-    data(){
-        return{
-            datatest:[{"x":1084,"y":15}]
+export default {
+    name: "ScatterPlot",
+    data() {
+        return {
+            datatest: [{ "x": 1084, "y": 15 }]
         }
     },
-    props:{
-        myPlotData : Array,
-        myChartID : String,
+    props: {
+        myPlotData: Array,
+        myChartID: String,
     },
-    created(){
+    created() {
 
     },
-    mounted(){
+    mounted() {
         //let localData = this.myPlotData;
-        let localData = this.datatest;
+        let localData = this.myPlotData;
         this.init(localData);
         this.drawplot(localData);
     },
-    watch:{
-        myPlotData(newval, oldval){
+    watch: {
+        myPlotData(newval, oldval) {
+            this.init(newval);
             this.drawplot(newval);
         }
     },
-    methods:{
-        init(data){
-            let id= '#'+this.myChartID;
-            var margin = {top: 10, right: 30, bottom: 20, left: 60},
+    methods: {
+        init(data) {
+            let id = '#' + this.myChartID;
+            var margin = { top: 10, right: 30, bottom: 20, left: 60 },
                 width = 1000 - margin.left - margin.right,
-                height = 150 - margin.top - margin.bottom;
+                height = 400 - margin.top - margin.bottom;
 
-            const svg = d3.select(id).select("svg")
-                        .attr("viewbox",[0, 0, width, height])
-                        .append("g")
-                        .attr("transform",
-                            "translate(" + margin.left + "," + margin.top + ")");
+                d3.select(id).select("svg").remove();
+            const svg = d3.select(id).append("svg")
+                .attr("viewbox", [0, 0, width, height])
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
             this.xs = d3.scaleLinear()
-                        .domain([0, 8000])
-                        .range([ 0, width ]);
+                .domain([0, 5000])
+                .range([0, width]);
 
-                    svg.append("g")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(d3.axisBottom(this.xs));
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(this.xs));
 
-                    // Add Y axis
+            // Add Y axis
             this.ys = d3.scaleLinear()
-                        .domain([0, 20])
-                        .range([ height, 0]);
+                .domain([0, 100])
+                .range([height, 0]);
 
-                    svg.append("g")
-                        .call(d3.axisLeft(this.ys));
+            svg.append("g")
+                .call(d3.axisLeft(this.ys));
 
-            this.dots = svg.append('g').selectAll("dot")
+            this.dots = svg.append('g').attr("id", this.myChartID + "scatter")
         },
-        drawplot(data){
-            let id= '#'+this.myChartID;
+        drawplot(data) {
+            let id = '#' + this.myChartID;
             let vueThis = this;
             //const svg = d3.select(id).select("svg");
 
             // Add dots
+            var t = d3.transition()
+                .duration(1000);
+            let selection = d3.select(id+"scatter").selectAll("dot")
+                .data(data, function (d) { return d.id; })
             
-            this.dots.data(data)
-                .enter()
+                selection.enter()
                 .append("circle")
-                .attr("cx", function (d) { return vueThis.xs(d.x); } )
-                .attr("cy", function (d) { return vueThis.ys(d.y); } )
+                .attr("cx", function (d) { return vueThis.xs(d.x); })
+                .attr("cy", function (d) { return vueThis.ys(d.y); })
                 .attr("r", 1.5)
                 .style("fill", "#69b3a2")
+                selection.exit().remove();
 
         }
     }
@@ -83,7 +91,7 @@ export default{
 </script>
 
 <style scoped>
-.card{
+.card {
     width: 100%;
     height: 100%;
 }
--- project/src/views/pages/trend.vue
+++ project/src/views/pages/trend.vue
@@ -1,7 +1,7 @@
 <template>
     <div class = "bottombar">
         <h3>ScatterPlot Here!</h3>
-        <ScatterPlot v-if="dataExists" myChartID="topscatter"></ScatterPlot>
+        <ScatterPlot v-if="dataExists" myChartID="topscatter" :myPlotData=scatterChartData></ScatterPlot>
     </div>
     <div class="bottombar">
         <h1>Number of commits </h1>
@@ -14,6 +14,7 @@ import BarChart from '../components/barchart.vue';
 import testData from "../../assets/data/test.json";
 import commitData from "../../assets/data/OSCI_commits_ranking_MTD.json"
 import ScatterPlot from "../components/scatter.vue";
+import repositoryCompositionData from "../../assets/data/Repository_Composition_Ranking_MTD.json"
 </script>
 <script>
 export default {
@@ -22,11 +23,14 @@ export default {
             dataExists: false,
             myBarData: Array,
             commitPieData: Array,
-            commit_by_company_vs_noncompany: Object,
+            repo_commit_by_month: Object,
+            repo_commit_by_company_by_month:Object,
             //data_person = d3.csvParse(FileAttachment().text(), d3.autoType)
             //data_title = d3.csvParse(FileAttachment().text(), d3.autoType)
             //actorGroups : Array,
             dateselected: ['2019-01', '2019-06'],
+            scatterChartData: Array,
+            
         }
     },
     components:{
@@ -38,6 +42,8 @@ export default {
     },
     created(){
         this.myBarData = testData.data;
+        [this.repo_commit_by_month, this.repo_commit_by_company_by_month] = this.parse_data()
+        this.extract_scatter_data()
         this.dataExists = true;
     },
     mounted(){
@@ -48,10 +54,89 @@ export default {
             console.log("Year changed!", data)
             this.dateselected[0] = data[0];
             this.dateselected[1] = data[1];
+            this.extract_scatter_data()
             //this.extract_company_commmit()
             //this.update_selected_company_info(this.selectedCompany)
             //console.log("Year updated", this.titleGroups, this.titleGroups_selected)
         },
+
+        extract_scatter_data() {
+            function groupBy(objectArray, property, transform, accumulate) {
+                return objectArray.reduce(function (acc, obj) {
+                    let key = obj[property]
+                    if (transform) {
+                        key = transform(key)
+                    }
+                    if (accumulate) {
+                        acc[key] = accumulate(acc[key], obj);
+                    } else {
+                        if (!acc[key]) {
+                            acc[key] = []
+                        }
+                        acc[key].push(obj)
+                    }
+                    return acc
+                }, {})
+            }
+            let total_commits = {};
+            let company_commits = {};
+            let months = 0;
+            for (const [month, commit_count] of Object.entries(this.repo_commit_by_month)) {
+                // console.log(commits)
+                let date = new Date(month);
+                if (date >= new Date(this.dateselected[0]) && date < new Date(this.dateselected[1])) {
+                    months += 1;
+                    for (const commit_entry of commit_count) {
+                        const repo = commit_entry["repo_name"]
+                        total_commits[repo] = (total_commits[repo] || 0) + commit_entry["Commits"]
+                        if (commit_entry["company"] != "Unknown") {
+                            company_commits[repo] = (company_commits[repo] || 0) + commit_entry["Commits"]
+                        }
+                    }
+                }
+            }
+
+            this.scatterChartData = Object.keys(total_commits).map((repo) => {
+                return {
+                    id: repo,
+                    x: total_commits[repo],
+                    y: company_commits[repo] / total_commits[repo] * 100
+                }
+            })
+        },
+
+        parse_data() {
+            function groupBy(objectArray, property, transform, accumulate) {
+                return objectArray.reduce(function (acc, obj) {
+                    let key = obj[property]
+                    if (transform) {
+                        key = transform(key)
+                    }
+                    if (accumulate) {
+                        acc[key] = accumulate(acc[key], obj);
+                    } else {
+                        if (!acc[key]) {
+                            acc[key] = []
+                        }
+                        acc[key].push(obj)
+                    }
+                    return acc
+                }, {})
+            }
+
+            let repo_commit_by_month = groupBy(repositoryCompositionData, "month", undefined, undefined);
+
+            let repo_commit_by_company_by_month = groupBy(repositoryCompositionData,
+                "month",
+                undefined,
+                undefined
+            )
+            for (const [key, value] of Object.entries(repo_commit_by_company_by_month)){
+                value.sort((a,b) => -a["Commits"] + b["Commits"])
+            }
+
+            return [repo_commit_by_month, repo_commit_by_company_by_month]
+        }
     }
 }
 </script>
